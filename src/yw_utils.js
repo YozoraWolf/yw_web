24b4f3e7f00434c395c0b436fe8aac61a75a2eac
YozoraWolf <yozorawolf@gmail.com>
2024-04-08 20:17:59 -0400

    fix: deleted some unused functions from utils

--- src/yw_utils.js
+++ src/yw_utils.js
@@ -1,20 +1,5 @@
 import anime from 'animejs';
 
-// load Local JSON Async
-export const loadLocalJSONA = async (path) => {
-    const response = await fetch(path);
-
-    if (!response.ok)
-        throw new Error(`Failed to load locale messages for ${locale}`);
-
-    return response.json();
-}
-
-// empties a JSON object
-export const emptyObject = (obj) => Object.fromEntries(
-        Object.entries(obj).map(([k, v]) => 
-        [k, typeof v === 'object' && v !== null ? emptyObject(v) : '']));
-
 // load default font
 export const loadDefaultFont = () => {
     document.documentElement.style.setProperty('--def-font',
